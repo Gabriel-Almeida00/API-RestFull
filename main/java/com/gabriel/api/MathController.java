a4ebf4cae2d01f41e6aba1c8718a38c380a1a0ce
gabriel <gaamdal557@gmail.com>
2023-07-04 21:09:02 -0300

    definindo a lógica da aplicação

--- main/java/com/gabriel/api/MathController.java
+++ main/java/com/gabriel/api/MathController.java
@@ -9,14 +9,24 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class MathController {
-    
+
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sum(@PathVariable(value = "numberOne") String numberOne,
                       @PathVariable(value = "numberTwo") String numberTwo
-    ) {
-        return 1D;
+    ) throws Exception {
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String number) {
+    }
+
+    private boolean isNumeric(String number) {
     }
 
 }
