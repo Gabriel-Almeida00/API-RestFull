95b1113d9f56887dd440a79c884bb22abea8d429
gabriel <gaamdal557@gmail.com>
2023-07-07 15:13:12 -0300

    configuração básica swagger

---
+++ main/java/com/gabriel/api/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.gabriel.api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAp() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("RestFull API with java and Spring")
+                        .version("v1")
+                        .description("some description about API")
+                        .termsOfService("https://github.com/Gabriel-Almeida00/API-Restfull")
+                        .license(new License()
+                                        .name("Apache 2.0")
+                                        .url("https://github.com/Gabriel-Almeida00/API-Restfull")));
+    }
+}
