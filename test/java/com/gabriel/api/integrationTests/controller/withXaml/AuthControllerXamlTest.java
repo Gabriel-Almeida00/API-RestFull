051b3b2af91ac67547ee28b6f2733757e1cc7c5c
gabriel <gaamdal557@gmail.com>
2023-07-19 17:52:59 -0300

    Testes de Integração do Controller de Login com YAML

--- test/java/com/gabriel/api/integrationTests/controller/withXaml/AuthControllerXamlTest.java
+++ test/java/com/gabriel/api/integrationTests/controller/withXaml/AuthControllerXamlTest.java
@@ -1,13 +1,14 @@
 package com.gabriel.api.integrationTests.controller.withXaml;
 
 import com.gabriel.api.configs.TestsConfig;
+import com.gabriel.api.integrationTests.controller.withXaml.mapper.YamlMapper;
 import com.gabriel.api.integrationTests.testContainers.AbstractIntegrationTest;
 import com.gabriel.api.integrationTests.vo.AccountCredentialsVO;
 import com.gabriel.api.integrationTests.vo.TokenVO;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import io.restassured.config.EncoderConfig;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
@@ -20,6 +21,11 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class AuthControllerXamlTest extends AbstractIntegrationTest {
 
     public static TokenVO tokenVO;
+    private static YamlMapper yamlMapper;
+    @BeforeAll
+    public static void setUp(){
+        yamlMapper = new YamlMapper();
+    }
 
     @Test
     @Order(1)
@@ -27,17 +33,22 @@ public class AuthControllerXamlTest extends AbstractIntegrationTest {
         AccountCredentialsVO user = new AccountCredentialsVO("leandro", "admin234");
 
         tokenVO = given()
+                .config(RestAssuredConfig.config()
+                                .encoderConfig(EncoderConfig.encoderConfig()
+                                        .encodeContentTypeAs(
+                                                TestsConfig.CONTENT_TYPE_YML, ContentType.TEXT)))
+                .accept(TestsConfig.CONTENT_TYPE_YML)
                 .basePath("/auth/signin")
                 .port(TestsConfig.SERVER_PORT)
                 .contentType(TestsConfig.CONTENT_TYPE_YML)
-                .body(user)
+                .body(user, yamlMapper)
                 .when()
                 .post()
                 .then()
                 .statusCode(200)
                 .extract()
                 .body()
-                .as(TokenVO.class);
+                .as(TokenVO.class, yamlMapper);
 
         assertNotNull(tokenVO.getAccessToken());
         assertNotNull(tokenVO.getRefreshToken());
@@ -49,6 +60,11 @@ public class AuthControllerXamlTest extends AbstractIntegrationTest {
         AccountCredentialsVO user = new AccountCredentialsVO("leandro", "admin234");
 
         var newTokenVO = given()
+                .config(RestAssuredConfig.config()
+                        .encoderConfig(EncoderConfig.encoderConfig()
+                                .encodeContentTypeAs(
+                                        TestsConfig.CONTENT_TYPE_YML, ContentType.TEXT)))
+                .accept(TestsConfig.CONTENT_TYPE_YML)
                 .basePath("/auth/refresh")
                 .port(TestsConfig.SERVER_PORT)
                 .contentType(TestsConfig.CONTENT_TYPE_YML)
@@ -60,7 +76,7 @@ public class AuthControllerXamlTest extends AbstractIntegrationTest {
                 .statusCode(200)
                 .extract()
                 .body()
-                .as(TokenVO.class);
+                .as(TokenVO.class, yamlMapper);
 
         assertNotNull(newTokenVO.getAccessToken());
         assertNotNull(newTokenVO.getRefreshToken());
--- test/java/com/gabriel/api/integrationTests/controller/withXaml/mapper/YamlMapper.java
+++ test/java/com/gabriel/api/integrationTests/controller/withXaml/mapper/YamlMapper.java
@@ -3,6 +3,7 @@ package com.gabriel.api.integrationTests.controller.withXaml.mapper;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import io.restassured.mapper.ObjectMapper;
 import io.restassured.mapper.ObjectMapperDeserializationContext;
 import io.restassured.mapper.ObjectMapperSerializationContext;
@@ -14,8 +15,8 @@ public class YamlMapper implements ObjectMapper {
     private com.fasterxml.jackson.databind.ObjectMapper objectMapper;
     protected TypeFactory typeFactory;
 
-    public YamlMapper(TypeFactory typeFactory) {
-        objectMapper = new com.fasterxml.jackson.databind.ObjectMapper();
+    public YamlMapper() {
+        objectMapper = new com.fasterxml.jackson.databind.ObjectMapper(new YAMLFactory());
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         typeFactory = TypeFactory.defaultInstance();
     }
