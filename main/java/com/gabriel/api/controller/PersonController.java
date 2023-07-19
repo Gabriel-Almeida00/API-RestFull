7e5ac1b9e90a77b9091e057606fd90e2299ea4c8
gabriel <gaamdal557@gmail.com>
2023-07-19 18:54:20 -0300

    teste de integração controller person

--- main/java/com/gabriel/api/controller/PersonController.java
+++ main/java/com/gabriel/api/controller/PersonController.java
@@ -97,19 +97,20 @@ public class PersonController {
         return service.update(person);
     }
 
-    @DeleteMapping("/{id}")
-    @Operation(summary = "deletes a person", description = "deletes a person",
+    @DeleteMapping(value = "/{id}")
+    @Operation(summary = "Deletes a Person",
+            description = "Deletes a Person by passing in a JSON, XML or YML representation of the person!",
             tags = {"People"},
             responses = {
-                    @ApiResponse(description = "No content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
                     @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
                     @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
                     @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
                     @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
             }
     )
-    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         service.delete(id);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 }
