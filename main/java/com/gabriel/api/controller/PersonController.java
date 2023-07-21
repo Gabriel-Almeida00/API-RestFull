c0415d1d46c227b58999701ea17e8d55045d936d
gabriel <gaamdal557@gmail.com>
2023-07-21 15:25:57 -0300

    paginação com pageable e query params

--- main/java/com/gabriel/api/controller/PersonController.java
+++ main/java/com/gabriel/api/controller/PersonController.java
@@ -10,10 +10,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("api/person/v1")
@@ -40,8 +41,12 @@ public class PersonController {
                     @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
             }
     )
-    public List<PersonVO> findAll() {
-        return service.findAll();
+    public ResponseEntity<Page<PersonVO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "limit", defaultValue = "12") Integer limit
+    ) {
+        Pageable pageable = PageRequest.of(page, limit);
+        return ResponseEntity.ok(service.findAll(pageable));
     }
 
     @GetMapping(value = "/{id}", produces = {"application/json", "application/xml", "application/x-yaml"})
