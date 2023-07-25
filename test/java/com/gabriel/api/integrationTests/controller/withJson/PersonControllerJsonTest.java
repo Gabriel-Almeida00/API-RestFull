2a9587127b8e3dd33020559fdcf7536340ecb769
gabriel <gaamdal557@gmail.com>
2023-07-24 21:33:21 -0300

    parâmetros de paginação e ajustes de assertions

--- test/java/com/gabriel/api/integrationTests/controller/withJson/PersonControllerJsonTest.java
+++ test/java/com/gabriel/api/integrationTests/controller/withJson/PersonControllerJsonTest.java
@@ -235,6 +235,7 @@ public class PersonControllerJsonTest extends AbstractIntegrationTest {
 
         var content = given().spec(specification)
                 .contentType(TestsConfig.CONTENT_TYPE_JSON)
+                .queryParams("page", 3,"size", 10,"direction","asc")
                 .when()
                 .get()
                 .then()
@@ -255,15 +256,15 @@ public class PersonControllerJsonTest extends AbstractIntegrationTest {
         assertNotNull(foundPersonOne.getGender());
         assertTrue(foundPersonOne.getEnabled());
 
-        assertEquals(1, foundPersonOne.getId());
+        assertEquals(61, foundPersonOne.getId());
 
-        assertEquals("Leandro", foundPersonOne.getFirstName());
-        assertEquals("Costa", foundPersonOne.getLastName());
-        assertEquals("Uberlândia - Minas Gerais - Brasil", foundPersonOne.getAddress());
-        assertEquals("Male", foundPersonOne.getGender());
+        assertEquals("Eugenie", foundPersonOne.getFirstName());
+        assertEquals("Bengochea", foundPersonOne.getLastName());
+        assertEquals("331 Acker Drive", foundPersonOne.getAddress());
+        assertEquals("Female", foundPersonOne.getGender());
 
 
-        PersonVO foundPersonSix = people.get(5);
+        PersonVO foundPersonSix = people.get(6);
 
         assertNotNull(foundPersonSix.getId());
         assertNotNull(foundPersonSix.getFirstName());
@@ -272,11 +273,11 @@ public class PersonControllerJsonTest extends AbstractIntegrationTest {
         assertNotNull(foundPersonSix.getGender());
         assertTrue(foundPersonSix.getEnabled());
 
-        assertEquals(6, foundPersonSix.getId());
+        assertEquals(16, foundPersonSix.getId());
 
-        assertEquals("Marcos", foundPersonSix.getFirstName());
-        assertEquals("Paulo", foundPersonSix.getLastName());
-        assertEquals("Patos de Minas - Minas Gerais - Brasil", foundPersonSix.getAddress());
+        assertEquals("Gearard", foundPersonSix.getFirstName());
+        assertEquals("Speller", foundPersonSix.getLastName());
+        assertEquals("555 Wayridge Junction", foundPersonSix.getAddress());
         assertEquals("Male", foundPersonSix.getGender());
     }
 
