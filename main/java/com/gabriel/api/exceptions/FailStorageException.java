252f7cd4c62725144a2c52b4e195d5b506e6df56
gabriel <gaamdal557@gmail.com>
2023-07-27 19:16:46 -0300

    exception e Vo Response

---
+++ main/java/com/gabriel/api/exceptions/FailStorageException.java
@@ -0,0 +1,17 @@
+package com.gabriel.api.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class FailStorageException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public FailStorageException(String message) {
+        super(message);
+    }
+
+    public FailStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
