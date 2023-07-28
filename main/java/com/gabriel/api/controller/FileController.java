25130b59afa200d3c815ed48abd447a3b35c4e71
gabriel <gaamdal557@gmail.com>
2023-07-28 19:59:48 -0300

    fileController método para upload de arquivo

---
+++ main/java/com/gabriel/api/controller/FileController.java
@@ -0,0 +1,37 @@
+package com.gabriel.api.controller;
+
+import com.gabriel.api.config.FileStorageConfig;
+import com.gabriel.api.data.vo.v1.UploadFileResponseVO;
+import com.gabriel.api.service.FileStorageService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.logging.Logger;
+
+@Tag(name = "File endpoint")
+@RestController
+@RequestMapping("/api/file/v1")
+public class FileController {
+
+    private Logger logger = Logger.getLogger(FileController.class.getName());
+
+    @Autowired
+    private FileStorageService fileService;
+
+    @PostMapping("/uploadFile")
+    public UploadFileResponseVO uploadFile(@RequestParam("file") MultipartFile file ){
+        logger.info("Storing file to disk");
+        var filename = fileService.storeFile(file);
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/api/file/v1/downloadFile/")
+                .path(filename)
+                .toUriString();
+        return new UploadFileResponseVO(filename, fileDownloadUri,file.getContentType(),file.getSize());
+    }
+}
