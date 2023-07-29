fb621afcd9490447a6a414dd65b1516ca119a3ec
gabriel <gaamdal557@gmail.com>
2023-07-28 21:27:26 -0300

    implmentando download de arquivos

--- main/java/com/gabriel/api/controller/FileController.java
+++ main/java/com/gabriel/api/controller/FileController.java
@@ -1,14 +1,15 @@
 package com.gabriel.api.controller;
 
-import com.gabriel.api.config.FileStorageConfig;
 import com.gabriel.api.data.vo.v1.UploadFileResponseVO;
 import com.gabriel.api.service.FileStorageService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -28,21 +29,41 @@ public class FileController {
     private FileStorageService fileService;
 
     @PostMapping("/uploadFile")
-    public UploadFileResponseVO uploadFile(@RequestParam("file") MultipartFile file ){
+    public UploadFileResponseVO uploadFile(@RequestParam("file") MultipartFile file) {
         logger.info("Storing file to disk");
         var filename = fileService.storeFile(file);
         String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
                 .path("/api/file/v1/downloadFile/")
                 .path(filename)
                 .toUriString();
-        return new UploadFileResponseVO(filename, fileDownloadUri,file.getContentType(),file.getSize());
+        return new UploadFileResponseVO(filename, fileDownloadUri, file.getContentType(), file.getSize());
     }
 
     @PostMapping("/uploadMultipleFiles")
-    public List<UploadFileResponseVO> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files ){
+    public List<UploadFileResponseVO> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
         logger.info("Storing files to disk");
-       return Arrays.asList(files)
-               .stream().map(file -> uploadFile(file))
-               .collect(Collectors.toList());
+        return Arrays.asList(files)
+                .stream().map(file -> uploadFile(file))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/downloadFile/{filename:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename, HttpServletRequest request) {
+        logger.info("Reading a file on disk");
+
+        Resource resource = fileService.loadFileAsResource(filename);
+        String contentType = "";
+
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (Exception e) {
+            logger.info("Could not determine file type");
+        }
+        if (contentType.isBlank()) contentType = "application/octet-stream";
+
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 }
